f83539253c4ca110b469e1152df4728eccc9815e
ZakuroSoda <reyesleeyh@gmail.com>
2023-08-10 22:10:56 +0800

    remove a stupid logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,12 +3,6 @@ import React from 'react';
 function Logo() {
   return (
     <>
-      <img
-        src="/images/job_penki_ojiisan.png"
-        className='img-fluid pb-2'
-        style={{ maxWidth: '20%', height: 'auto' }}
-        alt="logo"
-      />
       <h1 className='mb-3'>
         FixItAC++
       </h1>
