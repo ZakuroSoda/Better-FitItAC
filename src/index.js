f05dbeafcd0066516f70fb01d67767e4040335f2
ZakuroSoda <reyesleeyh@gmail.com>
2023-11-29 21:29:18 +0800

    Add React18 Test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// React 17
 import ReactDOM from "react-dom"
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -5,3 +6,15 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// React 18
+
+// import { createRoot } from 'react-dom/client';
+// import React from 'react';
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/js/bootstrap.bundle.min.js"
+// import App from "./App"
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App />);
