aab281e36400c1df899afab4af5b0d49a9c87c4a
ZakuroSoda <reyesleeyh@gmail.com>
2023-11-30 10:55:35 +0800

    select specifically instead of deleting the extra rows

--- server/util/getDefectReports.js
+++ server/util/getDefectReports.js
@@ -2,25 +2,20 @@ const { openDb } = require('./openDb');
 
 async function getDefectReports(schoolId) {
   const db = await openDb('./server/database.db');
-  let reports = await db.all('SELECT * FROM defect_reports WHERE school_id = ?', schoolId);
+  let reports = await db.all('SELECT uid, date, resolved_status, hidden_status, title, category, location FROM defect_reports WHERE school_id = ?', schoolId);
   for (let i = 0; i < reports.length; i++) {
     reports[i].date = new Date(reports[i].date).toISOString().split('T')[0];
     reports[i].resolved_status = reports[i].resolved_status === 0 ? 'Open' : 'Resolved';
-    delete reports[i].id;
-    delete reports[i].school_id;
-    delete reports[i].image_extension;
-    delete reports[i].description;
   }
   return reports;
 }
 
 async function getDefectReportsAll() {
   const db = await openDb('./server/database.db');
-  let reports = await db.all('SELECT * FROM defect_reports WHERE hidden_status = 0');
+  let reports = await db.all('SELECT uid, date, school_id, resolved_status, hidden_status, title, category, location, description, image_extension FROM defect_reports WHERE hidden_status = 0');
   for (let i = 0; i < reports.length; i++) {
     reports[i].date = new Date(reports[i].date).toISOString().split('T')[0];
     reports[i].resolved_status = reports[i].resolved_status === 0 ? 'Open' : 'Resolved';
-    delete reports[i].id;
   }
   return reports;
 }
--- server/util/getSuggestionReports.js
+++ server/util/getSuggestionReports.js
@@ -1,22 +1,21 @@
 const { openDb } = require('./openDb');
 
+// id, uid, date, school_id, title, description
+
 async function getSuggestionReports(schoolId) {
   const db = await openDb('./server/database.db');
-  let reports = await db.all('SELECT * FROM suggestion_reports WHERE school_id = ?', schoolId);
+  let reports = await db.all('SELECT uid, date, title, description FROM suggestion_reports WHERE school_id = ?', schoolId);
   for (let i = 0; i < reports.length; i++) {
     reports[i].date = new Date(reports[i].date).toISOString().split('T')[0];
-    delete reports[i].id;
-    delete reports[i].school_id;
   }
   return reports;
 }
 
 async function getSuggestionReportsAll() {
   const db = await openDb('./server/database.db');
-  let reports = await db.all('SELECT * FROM suggestion_reports');
+  let reports = await db.all('SELECT uid, date, school_id, title, description FROM suggestion_reports');
   for (let i = 0; i < reports.length; i++) {
     reports[i].date = new Date(reports[i].date).toISOString().split('T')[0];
-    delete reports[i].id;
   }
   return reports;
 }
